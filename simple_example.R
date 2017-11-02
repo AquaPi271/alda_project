12b05dbc934af95dd18da369b090f006eb6f5e90
Tim Ozimek <tozimek@qti.qualcomm.com>
2017-11-02 15:39:55 -0400

    Basic example of recommender lab.

---
+++ simple_example.R
@@ -0,0 +1,5 @@
+library("recommenderlab")
+m <- matrix(sample(c(as.numeric(0:5), NA), 50, replace=TRUE, prob=c(rep(.4/6,6),.6)), ncol = 10,
+            dimnames=list(user=paste("u", 1:5, sep=''), item=paste("i", 1:10, sep='')))
+r <- as(m, "realRatingMatrix")
+getRatingMatrix(r)
