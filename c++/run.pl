f93c6269a3e57681a9747dadb4d807968d4fb287
CosmicPi <tozimek@gmail.com>
2017-11-23 18:04:53 -0500

    UBCF is matching or slightly beating the base metric.  Next the data will be normalized to see if improvements happen.

---
+++ c++/run.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+my ($k);
+
+for( $k = 1; $k < 40; ++$k ) {
+    system( "bin/ubcf_metric.exe TOP_SAMPLES/TSAMPLE_M1000_U500 3 0.9 ".$k );
+}
+# seed = 1, 0.796379  k = 32  base = 0.810257
+# seed = 2, 0.922813  k = 25  base = 0.932677
+# seed = 3,                   base = 0.851404
--- c++/src/ubcf_metric.cc
+++ c++/src/ubcf_metric.cc
@@ -11,23 +11,43 @@
 #include "load_data.h"
 #include "ubcf_metric.h"
 
+struct nn {
+  uint32_t user_id;
+  uint32_t matched_count;
+  float    cos_dist;
+};
+
+uint32_t Gk = 1;
+float Gt = 0.95;
+
+void add_to_knn( uint32_t wuser_id, uint32_t matched_count, float cos_dist, std::vector<nn> & knn );
+bool find_top_knn_users_cosine_record_debug( std::map<uint16_t,std::map<uint32_t,uint8_t>> & train_movie_users,
+					     std::map<uint32_t,std::map<uint16_t,uint8_t>> & train_user_movies,
+					     std::map<uint32_t,std::map<uint16_t,uint8_t>> & test_users,
+					     uint32_t user_id,
+					     uint32_t requested_movie,
+					     std::vector<uint32_t> & matched_user_ids);
+
 bool find_top_knn_users_cosine_record( std::map<uint16_t,std::map<uint32_t,uint8_t>> & train_movie_users,
 				       std::map<uint32_t,std::map<uint16_t,uint8_t>> & train_user_movies,
 				       std::map<uint32_t,std::map<uint16_t,uint8_t>> & test_users,
 				       uint32_t user_id,
 				       uint32_t requested_movie,
-					 std::vector<uint32_t> & matched_user_ids);
+				       std::vector<uint32_t> & matched_user_ids);
 
 auto main( int argc, char **argv ) -> int {
 
-  if( argc != 4 ) {
-    std::cerr << "Usage: " << argv[0] << " <filename> <random_seed> <train_set_percent>" << std::endl;
+  if( argc != 6 ) {
+    std::cerr << "Usage: " << argv[0] << " <filename> <random_seed> <train_set_percent> <k> <k_threshold>" << std::endl;
     exit(1);
   }
 
   std::string s{ argv[1] };
   uint32_t random_seed = std::stoi( argv[2] );
   float train_set_percent = std::stof( argv[3] );
+  Gk = std::stoi( argv[4] );
+  Gt = std::stof( argv[5] );
+
 
   std::default_random_engine random_generator{random_seed};
 
@@ -112,20 +132,34 @@ auto main( int argc, char **argv ) -> int {
 
     std::vector<uint32_t> matched_users = {};
     
-    if( find_top_knn_users_cosine_record( train_movie_users, train_users, test_users,
-					  tu.first, test_movie,
-					  matched_users ) ) {
+    if( find_top_knn_users_cosine_record_debug( train_movie_users, train_users, test_users,
+						tu.first, test_movie,
+						matched_users ) ) {
       // Get matched_users ratings.
       //std::cout << "matched user = " << matched_users[0] << " ";
-      float rating = train_users[matched_users[0]][test_movie];     
-      auto test_rating = static_cast<float>((*item).second);
-      //std::cout << "rating = " << rating << " actual = " << test_rating << std::endl;
-      rmse_N += ((test_rating - rating)*(test_rating - rating));
-      ++rmse_D;
-      if( rmse_D > 0 ) {
-	std::cout << "RMSE = " << sqrt( rmse_N / rmse_D ) <<
-      	  "  cold start count = " << cold_start_count << " / " << count << std::endl;
-      }      
+      float ratings_sum = 0;
+      float ratings_count = 0;
+      for( auto & mu : matched_users ) {
+	ratings_sum += train_users[mu][test_movie];
+	ratings_count += 1.0f;
+      }
+      if( ratings_count <= 0 ) {
+	auto test_rating = static_cast<float>((*item).second);
+	auto test_movie = (*item).first;
+	auto avg_rating = train_movie_averages[test_movie];
+	rmse_N += ((test_rating - avg_rating)*(test_rating - avg_rating));
+	++rmse_D;
+      }	else {
+	float rating = ratings_sum / ratings_count;  
+	auto test_rating = static_cast<float>((*item).second);
+	//std::cout << "rating = " << rating << " actual = " << test_rating << std::endl;
+	rmse_N += ((test_rating - rating)*(test_rating - rating));
+	++rmse_D;
+	//if( rmse_D > 0 ) {
+	//	std::cout << "RMSE = " << sqrt( rmse_N / rmse_D ) <<
+	//	  "  cold start count = " << cold_start_count << " / " << count << std::endl;
+	//}
+      }
     } else { // Not one else rated!  Grab average as best recourse.      
       auto test_rating = static_cast<float>((*item).second);
       auto test_movie = (*item).first;
@@ -141,7 +175,7 @@ auto main( int argc, char **argv ) -> int {
   }
 
   if( rmse_D > 0 ) {
-    std::cout << "RMSE = " << sqrt( rmse_N / rmse_D ) << "  cold start count = " << cold_start_count << " / " << count << std::endl;
+    std::cout << "RMSE = " << sqrt( rmse_N / rmse_D ) << "  k = " << Gk << " cold start count = " << cold_start_count << " / " << count << std::endl;
   }
   
   return(0);
@@ -272,6 +306,111 @@ bool find_top_knn_users_cosine( std::map<uint16_t,std::map<uint32_t,uint8_t>> & 
   return( true );
 }
 
+bool find_top_knn_users_cosine_record_debug( std::map<uint16_t,std::map<uint32_t,uint8_t>> & train_movie_users,
+					     std::map<uint32_t,std::map<uint16_t,uint8_t>> & train_user_movies,
+					     std::map<uint32_t,std::map<uint16_t,uint8_t>> & test_users,
+					     uint32_t user_id,
+					     uint32_t requested_movie,
+					     std::vector<uint32_t> & matched_user_ids) {
+  // Get all training users that rated the requested movie.
+  std::vector<uint32_t> users_who_watched = {};
+  for( auto & train_mu : train_movie_users[requested_movie] ) {
+    users_who_watched.push_back( train_mu.first );
+  }
+  // Simplify lookup for the test user.  Just need to create movie/ratings hash for it.
+  std::map<uint16_t,uint8_t> test_user_movie = {};
+  for( auto & tum : test_users[user_id] ) {
+    if( tum.first != requested_movie ) {
+      test_user_movie[tum.first] = tum.second;
+    }
+  }
+  uint32_t target_test_rating = test_users[user_id][requested_movie];
+
+  auto tum_rated = test_user_movie.size();
+  if( tum_rated == 0 ) {
+    return( false );
+  }
+  //  uint32_t users_above_threshold = 0;
+  //
+  //bool highest_user_set = false;
+  //float highest_user_match_rating = 0;
+  //uint32_t highest_user_matched_rated_movies;
+  //uint32_t highest_user_id;
+
+  std::vector<nn> knn = {};
+  
+  for( auto & wuser_id : users_who_watched ) {
+    uint32_t AB = 0;
+    uint32_t A2 = 0;
+    uint32_t B2 = 0;
+    uint32_t matched_count = 0;
+    //std::cout << "train user = " << wuser_id << ":" << std::endl;
+    for( auto & test_movie : test_user_movie ) {
+      auto train_movie_rating = train_user_movies[wuser_id].find( test_movie.first );
+      if( train_movie_rating != train_user_movies[wuser_id].end() ) {
+	++matched_count;
+	uint32_t a = test_movie.second;
+	uint32_t b = (*train_movie_rating).second;
+	AB += a*b;
+	A2 += a*a;
+	B2 += b*b;
+	//std::cout << "     a = " << a << " b = " << b << std::endl;
+      }
+    }
+    float D = sqrt(A2) * sqrt(B2);
+    if( D != 0 ) {
+      float cos_dist = static_cast<float>(AB) / D;
+      add_to_knn( wuser_id, matched_count, cos_dist, knn );
+    }
+//    if( matched_count >= 100 ) {
+//      //std::cout << "matched_count = " << matched_count << " for user " << wuser_id << std::endl;
+//      float cos_dist = 0.0f;
+//      ++users_above_threshold;
+//      float D = sqrt(A2) * sqrt(B2);
+//      if( D != 0 ) {
+//	cos_dist = static_cast<float>(AB) / D;
+//	//std::cout << cos_dist << std::endl;
+//      }
+//      if( !highest_user_set ) {
+//	highest_user_set = true;
+//	highest_user_match_rating = cos_dist;
+//        highest_user_matched_rated_movies = matched_count;
+//	highest_user_id = wuser_id;
+//      } else {
+//	if( matched_count >= highest_user_matched_rated_movies ) {
+//	  highest_user_match_rating = cos_dist;
+//	  highest_user_matched_rated_movies = matched_count;
+//	  highest_user_id = wuser_id;
+//	} else if( (matched_count == highest_user_matched_rated_movies) && (cos_dist > highest_user_match_rating) ) {
+//	  highest_user_match_rating = cos_dist;
+//	  highest_user_matched_rated_movies = matched_count;
+//	  highest_user_id = wuser_id;
+//	} else if( cos_dist > highest_user_match_rating ) {
+//	  highest_user_match_rating = cos_dist;
+//	  highest_user_matched_rated_movies = matched_count;
+//	  highest_user_id = wuser_id;
+//	}
+//      }
+//    } else {
+//      return( false );
+//      //if( highest_user_match_rating < 1.01 ) {
+//      //	return( false );
+//      //}
+//    }
+    
+  }
+
+  //std::cout << "h match rating = " << highest_user_match_rating << " count = " << highest_user_matched_rated_movies << std::endl;
+
+  for( auto & n : knn ) {  
+    matched_user_ids.push_back( n.user_id );
+  }
+  
+  //std::cout << users_above_threshold << std::endl;
+
+  return( true );
+}
+
 bool find_top_knn_users_cosine_record( std::map<uint16_t,std::map<uint32_t,uint8_t>> & train_movie_users,
 				       std::map<uint32_t,std::map<uint16_t,uint8_t>> & train_user_movies,
 				       std::map<uint32_t,std::map<uint16_t,uint8_t>> & test_users,
@@ -355,11 +494,80 @@ bool find_top_knn_users_cosine_record( std::map<uint16_t,std::map<uint32_t,uint8
     
   }
 
-  std::cout << "h match rating = " << highest_user_match_rating << " count = " << highest_user_matched_rated_movies << std::endl;
+  //std::cout << "h match rating = " << highest_user_match_rating << " count = " << highest_user_matched_rated_movies << std::endl;
   
   matched_user_ids.push_back( highest_user_id );
   
   //std::cout << users_above_threshold << std::endl;
 
   return( true );
+}
+
+void add_to_knn_a( uint32_t wuser_id, uint32_t matched_count, float cos_dist, std::vector<nn> & knn ) {
+
+  uint32_t k = Gk;
+
+  if( knn.size() < k ) {
+    knn.push_back( {wuser_id, matched_count, cos_dist} );
+  } else {
+    // Ignore matched count for now!
+    float lowest_cos_dist = knn[0].cos_dist;
+    int32_t lowest_index = -1;
+    uint32_t index = 0;
+    for( auto & i : knn ) {
+      if( (lowest_cos_dist < i.cos_dist)  ) {
+	lowest_cos_dist = i.cos_dist;
+	lowest_index = index;
+      }
+      ++index;
+    }
+    if( lowest_index != -1 ) {
+      knn[lowest_index] = {wuser_id, matched_count, cos_dist};
+    }
+  }
+  
+}
+
+void add_to_knn_b( uint32_t wuser_id, uint32_t matched_count, float cos_dist, std::vector<nn> & knn ) {
+
+  uint32_t k = Gk;
+
+  if( knn.size() < k ) {
+    knn.push_back( {wuser_id, matched_count, cos_dist} );
+  } else { // Get lowest matched count.
+    uint32_t lowest_matched_count = knn[0].matched_count;
+    for( auto & i : knn ) {
+      if( lowest_matched_count < i.matched_count ) {
+	lowest_matched_count = i.matched_count;
+      }
+    }
+    if( matched_count < lowest_matched_count ) {
+      return;
+    }
+    // Of the lowest matched counts, get the lowest cos_dist.
+    float lowest_cos_dist = knn[0].cos_dist;
+    int32_t lowest_index = -1;
+    uint32_t index = 0;
+    for( auto & i : knn ) {
+      if( (lowest_cos_dist < i.cos_dist) && (lowest_matched_count == i.matched_count) ) {
+	lowest_cos_dist = i.cos_dist;
+	lowest_index = index;
+      }
+      ++index;
+    }
+    if( lowest_index != -1 ) {
+      knn[lowest_index] = {wuser_id, matched_count, cos_dist};
+    }
+  }
+  
+}
+
+void add_to_knn( uint32_t wuser_id, uint32_t matched_count, float cos_dist, std::vector<nn> & knn ) {
+
+  float t = Gt;
+
+  if( cos_dist > t ) {
+    knn.push_back( {wuser_id, matched_count, cos_dist} );
+  }
+  
 }
