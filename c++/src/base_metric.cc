9b6613a29a6e9e606e5c7b2a88d6de5ffd6f5c30
CosmicPi <tozimek@gmail.com>
2017-11-24 17:33:35 -0500

    Base metrics now does k-folds as well.
    Extracted fold generation into separate source.

--- c++/src/base_metric.cc
+++ c++/src/base_metric.cc
@@ -8,19 +8,26 @@
 #include <string>
 #include <vector>
 
+#include "fold_generation.h"
 #include "load_data.h"
 
 auto main( int argc, char **argv ) -> int {
 
-  if( argc != 4 ) {
-    std::cerr << "Usage: " << argv[0] << " <filename> <random_seed> <train_set_percent>" << std::endl;
+  if( argc != 5 ) {
+    std::cerr << "Usage: " << argv[0] << " <filename> <random_seed> <folds> <train_set_percent>" << std::endl;
     exit(1);
   }
 
   std::string s{ argv[1] };
   uint32_t random_seed = std::stoi( argv[2] );
-  float train_set_percent = std::stof( argv[3] );
+  uint32_t folds = std::stoi( argv[3] );
+  float train_set_percent = std::stof( argv[4] );
 
+  if( folds < 1 ) {
+    std::cout << "Fold count must be >= 1." << std::endl;
+    exit( 1 );
+  }
+  
   std::default_random_engine random_generator{random_seed};
   
   std::map<uint16_t,std::map<uint32_t,uint8_t>> movies;
@@ -37,67 +44,75 @@ auto main( int argc, char **argv ) -> int {
   
   std::random_shuffle( random_user_ids.begin(), random_user_ids.end() );
 
-  uint32_t train_user_count = std::round( train_set_percent * users.size() );
-  uint32_t test_user_count = users.size() - train_user_count;
+  uint32_t total_folds = folds;
 
-  auto train_start = random_user_ids.begin();
-  auto train_end   = random_user_ids.begin() + train_user_count;
-  auto test_start  = random_user_ids.begin() + train_user_count;
-  auto test_end    = random_user_ids.end();
+  std::vector<float> RMSE( total_folds, 0.0f );
   
-  std::vector<uint32_t> train_user_ids(train_start, train_end);
-  std::vector<uint32_t> test_user_ids(test_start, test_end);
-
-  //std::cout << "train_user_count  = " << train_user_count << std::endl;
-  //std::cout << "train vector size = " << train_user_ids.size() << std::endl;
-  //std::cout << "test_user_count   = " << test_user_count << std::endl;
-  //std::cout << "test vector size  = " << test_user_ids.size() << std::endl;
-
-  // Build train and test map.
-
-  std::map<uint32_t,std::map<uint16_t,uint8_t>> train_users;
-  std::map<uint32_t,std::map<uint16_t,uint8_t>> test_users;
-  std::map<uint16_t,std::vector<uint8_t>> train_movies;
-  std::map<uint16_t,float> train_movie_averages;
-  
-  for( auto & u : train_user_ids ) {
-    train_users[u] = users[u];
-    for( auto & m : users[u] ) {
-      train_movies[m.first].push_back(m.second);
-    }
-  }
-  for( auto & u : test_user_ids ) {
-    test_users[u] = users[u];
-  }
-  for( auto & m : train_movies ) {
-    uint32_t sum = 0;
-    for( auto & r : m.second ) {
-      sum += r;
-    }
-    train_movie_averages[m.first] = static_cast<float>(sum) / static_cast<float>( m.second.size() );
-  }
-
-  // RMSE start!
-
-  float rmse_N = 0.0f;
-  float rmse_D = 0.0f;
-
-  for( auto & tu : test_users ) {
-    auto movies_rated = tu.second.size();
-    std::uniform_int_distribution<uint32_t> dist(0, movies_rated - 1);
-    auto random_offset = dist(random_generator);
-    auto item = tu.second.begin();
-    std::advance( item, random_offset );
-    //std::cout << "movie " << (*item).first << " : " << static_cast<uint32_t>((*item).second) << std::endl;
-    auto test_rating = static_cast<float>((*item).second);
-    auto test_movie = (*item).first;
-    auto avg_rating = train_movie_averages[test_movie];
-    rmse_N += ((test_rating - avg_rating)*(test_rating - avg_rating));
-    ++rmse_D;
+  for( uint32_t fold = 0; fold < total_folds; ++fold ) {
     
+    std::vector<uint32_t> train_user_ids = {};
+    std::vector<uint32_t> test_user_ids  = {};
+    
+    if( total_folds == 1 ) { // Going into percentage mode for test generation.
+      extract_fold_user_ids_percentage_method( random_user_ids, train_set_percent, train_user_ids, test_user_ids );
+    } else {    
+      extract_fold_user_ids( random_user_ids, fold, total_folds, train_user_ids, test_user_ids );
+    }
+
+    // Build train and test map.
+
+    std::map<uint32_t,std::map<uint16_t,uint8_t>> train_users;
+    std::map<uint32_t,std::map<uint16_t,uint8_t>> test_users;
+    std::map<uint16_t,std::vector<uint8_t>> train_movies;
+    std::map<uint16_t,float> train_movie_averages;
+    
+    for( auto & u : train_user_ids ) {
+      train_users[u] = users[u];
+      for( auto & m : users[u] ) {
+	train_movies[m.first].push_back(m.second);
+      }
+    }
+    for( auto & u : test_user_ids ) {
+      test_users[u] = users[u];
+    }
+    for( auto & m : train_movies ) {
+      uint32_t sum = 0;
+      for( auto & r : m.second ) {
+	sum += r;
+      }
+      train_movie_averages[m.first] = static_cast<float>(sum) / static_cast<float>( m.second.size() );
+    }
+
+    // RMSE start!
+
+    float rmse_N = 0.0f;
+    float rmse_D = 0.0f;
+    
+    for( auto & tu : test_users ) {
+      auto movies_rated = tu.second.size();
+      std::uniform_int_distribution<uint32_t> dist(0, movies_rated - 1);
+      auto random_offset = dist(random_generator);
+      auto item = tu.second.begin();
+      std::advance( item, random_offset );
+      //std::cout << "movie " << (*item).first << " : " << static_cast<uint32_t>((*item).second) << std::endl;
+      auto test_rating = static_cast<float>((*item).second);
+      auto test_movie = (*item).first;
+      auto avg_rating = train_movie_averages[test_movie];
+      rmse_N += ((test_rating - avg_rating)*(test_rating - avg_rating));
+      ++rmse_D; 
+    }
+
+    RMSE[fold] = sqrt( rmse_N / rmse_D );
+
   }
 
-  std::cout << "RMSE = " << sqrt( rmse_N / rmse_D ) << std::endl;
+  float RMSE_sum = 0.0f;
+
+  for( auto & rmse : RMSE ) {
+    RMSE_sum += rmse;
+  }
+  
+  std::cout << "RMSE = " << (RMSE_sum / static_cast<float>( total_folds ))<< " folds = " << total_folds << std::endl;
   
   return(0);
 
